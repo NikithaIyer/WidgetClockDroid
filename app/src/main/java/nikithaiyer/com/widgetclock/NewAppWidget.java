7d5cb24590a773c6ab546f18ecd3f004fa0a5089
Nikitha Iyer <nikita9.iyer@gmail.com>
2015-06-16 22:05:03 +0530

    First commit

---
+++ app/src/main/java/nikithaiyer/com/widgetclock/NewAppWidget.java
@@ -0,0 +1,13 @@
+package nikithaiyer.com.widgetclock;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class NewAppWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        int id = appWidgetIds[0];
+
+    }
+}
